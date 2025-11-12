78ac373e16fa25bb0398d13093923d4bd7daa14e
vinaykumar2004vinay <vinaykumardasari2004@gmail.com>
2025-11-12 10:57:02 +0530

    JAVASCRIPT

---
+++ Class-18/Import&Export/central.js
@@ -0,0 +1,7 @@
+let tax=18
+
+function login(){
+    console.log("Login Success")
+}
+//export default tax; //default export
+export {tax,login}  //Named Export
---
+++ Class-18/Import&Export/ka.js
@@ -0,0 +1,4 @@
+//import tax from './central.js'
+import {tax,login} from './central.js'
+console.log(tax)
+login()
